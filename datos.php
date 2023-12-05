b52c87c2a97307af5dba53f5cca7837443c1aa9d
lbellido <lucianobellido@yahoo.com.ar>
2023-12-05 17:24:51 -0300

    dos

---
+++ datos.php
@@ -0,0 +1,45 @@
+
+<?php
+function datos_productos(){
+
+return [
+    [
+        'id'=>'1',
+        'nombre'=> 'Casual Hush Puppies Arthur II',
+        'precio'=> 3500, 
+        'imagen'=>'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2023/9/22/8856286.jpg'
+
+    ],
+    [
+        'id'=>'2',
+        'nombre'=> 'Casual Hush Puppies Picker',
+        'precio'=> 4200, 
+        'imagen'=>'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2022/7/27/7583450.jpg'
+
+    ],
+    [
+        'id'=>'3',
+        'nombre'=> 'Casual Hush Puppies Serrano',
+        'precio'=> 1373, 
+        'imagen'=>'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2023/7/25/8638679.jpg'
+
+    ],
+    [
+        'id'=>'4',
+        'nombre'=> 'Zapatillas Vans M WARD',
+        'precio'=> 2500, 
+        'imagen'=>'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2023/3/14/8175792.jpg'
+
+    ],
+    [
+        'id'=>'5',
+        'nombre'=> 'Casual Hush Puppies Arthur II',
+        'precio'=> 14000, 
+        'imagen'=>'https://mmgrim2.azureedge.net/MediaFiles/Grimoldi/2023/3/14/8175904.jpg'
+
+    ]
+];
+
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Listado de productos </title>
+</head>
+<body>
+    <h1> Productos</h1>
+
+    Deberán crear dos archivos, uno que contenga una función con un array de datos, 
+    y el otro archivo que llame e incorpore con lo visto en clase a la función y se muestre los datos en una Card.
+<div>
+  
+<div class="container-fluid">
+<div class="row"> 
+ <!-- <div class="card-group ">  -->
+
+<?php
+
+require("datos.php");
+
+$array = datos_productos();
+var_dump($array);
+     
+
+
+foreach($array as $e){
+ //  echo $e['nombre'] ;
+  ?>
+   
+
+  <div class="col-sm-4">
+    <div class="card">
+    <img src="<?php echo $e['imagen'] ?>" class="card-img-top " style="max-width: 200px; max-height: 200px;" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"> Código: <?php echo $e['id'] ?></h5>
+        <p class="card-text"> Artículo: <?php echo $e['nombre'] ?></p>
+        <p class="card-text"> Precio: <?php echo $e['precio'] ?></p>
+        <!-- <a href="#" class="btn btn-primary"> < ?php echo $e['nombre'] ?></a> -->
+      </div>
+    </div>
+  </div>
+
+    <?php
+
+    }
+ ?>
+</div>
+</div>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
